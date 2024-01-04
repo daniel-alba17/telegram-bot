a58dcacb2926d48c2865c5aac186a785b5e9436e
Daniel Alba <daniel.alba@alternova.com>
2024-01-03 22:30:29 -0500

    feat: implement telegram library

--- src/main.py
+++ src/main.py
@@ -1,29 +1,14 @@
 from letterboxd import LetterBoxd
-from telegram import Telegram
-
-offset = 0
-telegram = Telegram()
+from decouple import config
+import telebot
 
 
-def send_text(chat_id, msg):
-    telegram.send_message(message=msg, chat_id=chat_id)
+bot = telebot.TeleBot(config("TELEGRAM_TOKEN"), parse_mode=None)
 
 
-while True:
-    updates = telegram.get_updates(offset)
+@bot.message_handler(commands=["start"])
+def send_welcome(message):
+    bot.reply_to(message, LetterBoxd.get_watchlist())
 
-    if updates:
-        if offset == 0:
-            offset = updates[0]["update_id"]
 
-        user_id = updates[0]["message"]["from"]["id"]
-        chat_id = updates[0]["message"]["chat"]["id"]
-        message_text = updates[0]["message"]["text"]
-
-        if message_text.startswith("/"):
-            if message_text == "/start":
-                coincidences = "\n".join(LetterBoxd.get_watchlist())
-
-                send_text(chat_id, coincidences)
-
-        offset += 1
+bot.infinity_polling()
