cd700a3342c73d2007447a251cdb2df00a2cfe68
Daniel Alba <daniel.alba@alternova.com>
2024-01-03 22:29:45 -0500

    feat: change the way max pages were obtain

--- src/letterboxd.py
+++ src/letterboxd.py
@@ -1,10 +1,22 @@
 import logging
 import requests
 from bs4 import BeautifulSoup
-import json
 
 
 BASEURL = "https://letterboxd.com/"
+
+
+def make_request(url):
+    return requests.get(url)
+
+
+def get_max_pages(username):
+    response = make_request(f"{BASEURL}{username}/watchlist/")
+    soup = BeautifulSoup(response.text, "html.parser")
+    pagination = soup.find("div", class_="pagination").find_all("li")
+    max_pages = int(pagination[-1].find("a").text)
+
+    return max_pages
 
 
 class LetterBoxd:
@@ -12,54 +24,32 @@ class LetterBoxd:
     def get_watchlist(usernames=["daniel_alba", "malejaa"]):
         result_global = []
         for username in usernames:
+            print("username")
             result = []
             page = 1
-            max_pages = 1
-            print("======USERNAME", username)
-            while page <= max_pages:
-                print("paginas", page)
-                print("maximo de plaginas", max_pages)
-                response = requests.get(f"{BASEURL}{username}/watchlist/page/{page}")
+            max_pages = get_max_pages(username)
 
-                # Verifica si la solicitud fue exitosa (código de estado 200)
+            while page <= max_pages:
+                response = make_request(f"{BASEURL}{username}/watchlist/page/{page}")
+
                 if response.status_code == 200:
-                    # Parsea el contenido HTML con BeautifulSoup
                     soup = BeautifulSoup(response.text, "html.parser")
 
                     ul_element = soup.find("ul", class_="poster-list")
-                    pagination = soup.find("div", class_="pagination")
-                    max_pages = len(pagination.find_all("li"))
-                    # print(ul_element)
 
-                    # Verifica si se encontró el elemento
                     if ul_element:
-                        # Obtiene todos los elementos dentro del <ul>
-                        lista_de_elementos = ul_element.find_all("img", class_="image")
-                        # print(len(lista_de_elementos))
+                        images = ul_element.find_all("img", class_="image")
 
-                        # Imprime o manipula los elementos según tus necesidades
-                        for elemento in lista_de_elementos:
-                            result.append(elemento.get("alt"))
-                            pass
-
-                    else:
-                        print("No se encontró el <ul> con la clase 'poster-list'.")
+                        for image in images:
+                            result.append(image.get("alt"))
 
                     page += 1
-
-                else:
-                    print(
-                        "Error al realizar la solicitud HTTP. Código de estado:",
-                        response.status_code,
-                    )
 
             result_global.append(result)
 
         set1 = set(result_global[0])
         set2 = set(result_global[1])
 
-        coincidencias = set1.intersection(set2)
+        coincidences = set1.intersection(set2)
 
-        lista_coincidencias = list(coincidencias)
-
-        return lista_coincidencias
+        return "\n".join(coincidences)
